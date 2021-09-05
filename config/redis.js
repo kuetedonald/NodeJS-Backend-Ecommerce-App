2cdc92cd89111603f8bbb6f0f93a13a094fa4efd
Herman Demsong <hdemsongtsamo@gmail.com>
2021-09-05 13:57:09 +0100

    fix redis export

--- config/redis.js
+++ config/redis.js
@@ -2,9 +2,9 @@ const Config= require("./config.js");
 const redis = require('redis');
 
 const client = () => {
-    return redis.createClient({
+    return redis.createClient('redis://kuetedonald:123@Donald@redis-15074.c289.us-west-1-2.ec2.cloud.redislabs.com:15074', {
         host: Config.development.REDIS_HOST,
-        port: Config.development.REDIS_PORT
+        port: Config.development.REDIS_PORT,
     });
 };
 module.exports = client();
